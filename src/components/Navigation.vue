72298dd6df89b4c40cb8371c91b502cffcf1fd32
thegirlfromipanema <mayanovarini@gmail.com>
2017-12-13 00:55:34 -0800

    Create navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <a href="#">Some Link</a>
+    <a href="#">Some Other Link</a>
+    <a href="#">Friends({{FriendStore.friends.length}})</a>
+  </nav>
+</template>
+
+<script>
+import FriendStore from "../stores/FriendStore"
+
+export default {
+  data() {
+    return {
+      FriendStore: FriendStore.data
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+nav {
+  background: pink;
+  color: grey;
+  margin-bottom: 20px;
+  padding: 20px 10%;
+  text-align: center;
+  width: 100%;
+}
+
+a {
+  color: white;
+}
+
+</style>
